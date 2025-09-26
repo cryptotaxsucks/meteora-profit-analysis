797638e60c68e023fa4bd401b0545557b2a18744
cryptotaxsucks <159999602+cryptotaxsucks@users.noreply.github.com>
2025-09-26 20:22:27 +1000

    Update worker-loader.d.ts
    vibe debugging

--- types/worker-loader.d.ts
+++ types/worker-loader.d.ts
@@ -1,4 +1,9 @@
+// types/worker-loader.d.ts
 declare module "*.worker.ts" {
+  const WorkerFactory: { new (): Worker };
+  export default WorkerFactory;
+}
+declare module "worker-loader!*" {
   const WorkerFactory: { new (): Worker };
   export default WorkerFactory;
 }
